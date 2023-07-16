a5784e41bd573c2cfd3427916ea8d43cffc33349
thayanequintanilha <thayaniquintanilha2@gmail.com>
2023-07-16 15:05:04 -0300

    feat: evento de marcação no mapa

--- src/pages/map/MapPage.tsx
+++ src/pages/map/MapPage.tsx
@@ -1,16 +1,29 @@
 import React, { useState } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 import '../../styles/Map/MapPage.css'
 
 function MapPage() {
+    const [clickedLatLng, setClickedLatLng] = useState(null);
+
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: "AIzaSyC77g2ossazn1bq9HOm_sWUlhsmmepS-qk"
     })
 
+    const handleClick = (event) => {
+        const latLng = {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng()
+          };
+
+        setClickedLatLng(latLng);
+
+        console.log(clickedLatLng)
+    }
+
     const local = { lat: -22.748030, lng: -42.897633 };
-    
+
     return  (
         <div className='map'>
             {isLoaded ? (
@@ -18,7 +31,13 @@ function MapPage() {
                 mapContainerStyle={{ width: '100%', height: '100%' }}
                 center={local}
                 zoom={15}
+                onClick={handleClick}
               >
+                {clickedLatLng ? (
+                    <Marker position={clickedLatLng} options={{
+                    label: {text: 'teste', className: "map-marker"},
+                }} />
+                ) : ''}
               </GoogleMap>
             ) : ''}
         </div>
